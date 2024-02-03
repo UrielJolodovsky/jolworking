01aca20db4742cf174afba3aa31a7470fd0a581f
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-03 15:38:40 -0300

    trying login with Google

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,20 +1,12 @@
-"use client"
-
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Router } from "next/router";
+'use client'
+import { signIn } from "next-auth/react";
 
 export default function Header() {
-  const router = useRouter();
-  
-  function handleClick() {
-    router.push("/login");
-  }
 
   return (
     <header className="flex justify-center items-center z-10 relative p-5">
       <h1 className="text-white text-xl italic">Jolworking</h1>
-      <button onClick={handleClick} className="absolute right-3 sm:right-10 px-3 py-1.5 text-black bg-green-600 rounded-md text-sm transition hover:bg-green-400">
+      <button onClick={singIn} className="absolute right-3 sm:right-10 px-3 py-1.5 text-black bg-green-600 rounded-md text-sm transition hover:bg-green-400">
         Sign up
       </button>
       <hr className="bg-white" />
@@ -22,3 +14,10 @@ export default function Header() {
   );
 }
 
+async function singIn() { 
+  await signIn("google", { callbackUrl: "http://localhost:3000" }).then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+  })
+}
