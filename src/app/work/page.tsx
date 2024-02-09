ff0b2c7677a4fdf50b466a6d0c9466a5e916958d
Uriel Jolodovsky <46960341@est.ort.edu.ar>
2024-02-09 02:55:27 -0300

    Header with login implemented almost finished

---
+++ src/app/work/page.tsx
@@ -0,0 +1,7 @@
+
+
+export default function Work() {
+    return (
+        <div className="text-white">hola</div>
+    )
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 'use client'
-import { signIn, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Suspense } from "react";
 
 export default function Header() {
 
@@ -7,10 +8,27 @@ export default function Header() {
 const { data: session } = useSession()
   return (
     <header className="flex justify-center items-center z-10 relative p-5">
+      <nav className="absolute left-10 flex justify-center items-center gap-x-5">
+        <a href="/" className="text-white/60 hover:text-white/80 transition">Home</a>
+        <a href="/dashboard" className="text-white/60 hover:text-white/80 transition">Dashboard</a>
+        <a href="/work" className="text-white/60 hover:text-white/80 transition">New work</a>
+      </nav>
       <h1 className="text-white text-xl italic">Jolworking</h1>
-      <button onClick={singIn} className="absolute right-3 sm:right-10 px-3 py-1.5 text-black bg-green-600 rounded-md text-sm transition hover:bg-green-400">
+      {/* <Suspense fallback={""}> */}
+      {session?.user ? (
+        <div className="absolute right-3 sm:right-10 flex justify-center items-center gap-x-5">
+          <img src={session?.user.image!} alt="user" className="w-10 h-10 rounded-full" />
+          <h3 className="text-white text-sm lg:block hidden">{session?.user.name}</h3>
+          <button onClick={logOut} className="sm:right-20 px-3 py-1.5 text-black bg-green-600 rounded-md text-sm transition hover:bg-green-400">
+            Log out
+          </button>
+        </div>
+      ) : (
+        <button onClick={singIn} className="absolute right-3 sm:right-10 px-3 py-1.5 text-black bg-green-600 rounded-md text-sm transition hover:bg-green-400">
         Sign up
-      </button>
+        </button>
+      )}
+      {/* </Suspense> */}
       <hr className="bg-white" />
     </header>
   );
@@ -18,6 +36,14 @@ const { data: session } = useSession()
 
 async function singIn() { 
   await signIn("google", { callbackUrl: "http://localhost:3000" }).then((res) => {
+    console.log(res)
+}).catch((err) => {
+    console.log(err)
+  })
+}
+
+async function logOut() {
+  await signOut({callbackUrl: "http://localhost:3000"}).then((res) => {
     console.log(res)
 }).catch((err) => {
     console.log(err)
